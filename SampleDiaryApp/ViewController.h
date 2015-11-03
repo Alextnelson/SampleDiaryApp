71338ccde6c9f1224253a8691da0dd7d111d846e
Alextnelson <alexander.thomas.nelson@gmail.com>
2015-11-03 00:32:28 -0600

    Initial Commit

---
+++ SampleDiaryApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleDiaryApp
+//
+//  Created by Alexander Nelson on 11/3/15.
+//  Copyright © 2015 Jetwolfe Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
